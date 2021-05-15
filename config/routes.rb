9cc1e608e0d07578aa51423af282cc2fd3188274
ヨシダタイキ <abc521fjr7@gmail.com>
2021-05-15 21:22:56 +0900

    [add]recipesモデル

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/edit'
   root to: 'home#top'
   devise_for :users
   resource :user, except: [:new, :create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_080602) do
+ActiveRecord::Schema.define(version: 2021_05_15_122208) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
